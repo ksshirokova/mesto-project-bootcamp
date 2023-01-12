c963898f81819fc6c4929ae90f28074eaa4def5a
Kseniia Shirokova <ksirokova85@gmail.com>
2023-01-12 11:21:16 +0300

    fix: исправить кнопку лайка и добавление карточек

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const initialCards = [
 const addCardButton = document.querySelector('.popup__save-button_card');
 const cardsContainer = document.querySelector('.elements__list');
 const popupElement = document.querySelector('.popup');
-const editPopup = document.querySelector('.popup_name_add-name');
+const profilePopup = document.querySelector('.popup_name_add-name');
 const addPopup = document.querySelector('.popup_name_add-card');
 const editPopupButton = document.querySelector('.profile__edit-button');
 const closePopupElement = document.querySelector('.popup__close-button');
@@ -38,13 +38,13 @@ const formElement = document.querySelector('.popup__form');
 const formAddCard = document.querySelector('.popup__form_type_add-card');
 const formAddName = document.querySelector('.popup__form_type_add-name');
 const nameInput = document.querySelector('.popup__name');
+const popupInput = document.querySelector('.popup__input')
 const titleImage = document.querySelector('.popup__job_image-link');
 const nameImage = document.querySelector('.popup__name_image-text');
 const jobInput = document.querySelector('.popup__job');
-const saveButton = document.querySelector('.popup__save-button');
 const nameOfUser = document.querySelector('.profile__title-info');
 const jobOfUser = document.querySelector('.profile__subtitle-info');
-const imagePopup = document.querySelector('.popup__card-image');
+const imagePopup = document.querySelector('.popup_card-image');
 const closeImagePopupButton = document.querySelector('.popup__close-button_third');
 
 const popupImage = document.querySelector('.popup__background-picture');
@@ -54,16 +54,44 @@ const cardTemplate = document.querySelector('#element');
 
 const renderCards = (elements) => {
 
-  const cards = elements.map((card) => {
-    return addNewCard(card);
+  
+  const cards = elements.map(addNewCard);
 
-
-  });
   cardsContainer.prepend(...cards);
 
 };
 
 renderCards(initialCards);
+
+// Функция, которая добавляет класс с ошибкой
+const showInputError = (element) => {
+  element.classList.add('popup__input_type_error');
+};
+
+// Функция, которая удаляет класс с ошибкой
+const hideInputError = (element) => {
+  element.classList.remove('popup__input_type_error');
+};
+
+// Функция, которая проверяет валидность поля
+const isValid = () => {
+  if (!popupInput.validity.valid) {
+    // Если поле не проходит валидацию, покажем ошибку
+    showInputError(popupInput);
+  } else {
+    // Если проходит, скроем
+    hideInputError(popupInput);
+  }
+};
+
+// closePopupElement.forEach((button)=>{
+//   const popup = button.closest('.popup');
+//   button.addEventListener('click', () => closePopup(popup));
+// });
+
+
+// Вызовем функцию isValid на каждый ввод символа
+popupInput.addEventListener('input', isValid); 
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
@@ -72,7 +100,7 @@ function closePopup(popup) {
   popup.classList.remove('popup_opened');
 };
 editPopupButton.addEventListener("click", function () {
-  openPopup(editPopup);
+  openPopup(profilePopup);
 });
 addCardPopupButton.addEventListener("click", function () {
   openPopup(addPopup);
@@ -95,8 +123,7 @@ closeImagePopupButton.addEventListener('click', function () {
 
 
 
-
-function addNewCard(card) {
+function createCard (item){
   const cardElement = cardTemplate.cloneNode(true).content;
 
   const image = cardElement.querySelector('.element__background-picture');
@@ -104,14 +131,14 @@ function addNewCard(card) {
   const heartButtonElement = cardElement.querySelector('.element__heart-button');
   const deleteButtonElement = cardElement.querySelector('.element__delete-button');
 
-  image.src = card.link;
-  image.alt = card.name;
-  name.textContent = card.name;
+  image.src = item.link;
+  image.alt = item.name;
+  name.textContent = item.name;
 
   heartButtonElement.addEventListener('click', function (evt) {
     const eventTarget = evt.target;
     eventTarget.classList.toggle('element__heart-button_on');
-    eventTarget.classList.toggle('element__heart-button')
+    
   })
 
   deleteButtonElement.addEventListener('click', function (evt) {
@@ -120,12 +147,20 @@ function addNewCard(card) {
 
 
 
-  image.addEventListener('click', () => handleImagePreview(card))
+  image.addEventListener('click', () => handleImagePreview(item))
 
-  cardsContainer.prepend(cardElement);
+  // cardsContainer.prepend(cardElement);
   return cardElement;
-
 }
+
+
+
+function addNewCard(card) {
+  const cardElement = createCard(card)
+  cardsContainer.prepend(cardElement);
+}
+
+
 
 function handleImagePreview(card) {
   popupImage.src = card.link;
@@ -155,8 +190,9 @@ formAddCard.addEventListener('submit', function (event) {
     
   })
 
-  titleImage.value = '';
-  nameImage.value = '';
+  // titleImage.value = '';
+  // nameImage.value = '';
+  event.target.reset()
  
   closePopup(addPopup);
 })
